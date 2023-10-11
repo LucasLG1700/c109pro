7ade9c32ac8c174f1e611c6d1d2f81226024b792
LucasLG1700 <126296384+LucasLG1700@users.noreply.github.com>
2023-10-11 19:38:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true, video:false});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/BteE39KzW/model.json', { probabilityThreshold: 0.7 } ,modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+
+var dog = 0;
+var cat = 0;
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+  }
+  document.getElementById("result_label").innerHTML = 'Som detectado de  - '+ results[0].label;
+  document.getElementById("result_count").innerHTML = 'Cachorro detectado - '+dog+ ' Gato detectado - '+cat;
+  document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+  document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+  img = document.getElementById('peppa');
+
+  if (results[0].label == "au au") {
+    img.src = 'th.jpeg';
+    dog = dog+1;
+  } else if (results[0].label == "miau") {
+    img.src = 'th (1).jpeg';
+    cat = cat + 1;
+  } else{
+    img.src = 'th.jpg';
+  }
+}
